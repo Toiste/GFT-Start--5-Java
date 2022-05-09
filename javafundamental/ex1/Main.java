01b6bfeec13a07e9131127e43b347c846b91a30a
Marllon Ribeiro <Marlinn.bb@gmail.com>
2022-05-08 22:16:53 -0300

    JavaFundamental

---
+++ javafundamental/ex1/Main.java
@@ -0,0 +1,53 @@
+package javafundamental.ex1;
+
+/**
+ * Classe de exemplo para o exercicio da Aula 1 de Variáveis, Tipos de Dado e Operadores aritméticos.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i;
+        // int i;
+        int I;
+        // int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        // j = 15;
+        int asgfsd2324;
+        // int asasf23 asam2;
+        int as2sfsfds_sadnsadsa = 10;
+        // int as2sfsf%ds_sadnsadsa = 10;
+
+        asgfsd2324 = 100;
+        asgfsd2324 = 10;
+
+        int quantidadeProduto = 50;
+        // int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        // final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        // int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asgfsd2324);
+        System.out.println(as2sfsfds_sadnsadsa);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
